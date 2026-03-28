43699799c0ab2cf2550d21200c3bad4150ab36fd
ThainanViniciusKatchan <thaynanvinicyus1@gmail.com>
2026-03-27 20:43:45 -0400

    primeira implementação das novas funções com orientação a objeto

--- newUtils/StartUp.py
+++ newUtils/StartUp.py
@@ -3,25 +3,25 @@ from utils.model import json_path, load_config, save_config
 JSON_PATH = json_path()
 CONFIG = load_config()
 
-
-class StartUp:
-    def __init__(self, enabled: bool = False):
-        self._enabled = enabled
+class StartUpSys:
+    def __init__(self, value: bool = False):
+        self._value = value
 
     @property
     def enabled(self):
-        return self._enabled
+        return self._value
 
     @enabled.getter
     def GetEnabled(self):
         return CONFIG["Startup"]
 
     @enabled.setter
-    def enabled(self, enabled: bool):
-        CONFIG["Startup"] = enabled
+    def enabled(self, value: bool):
+        CONFIG["Startup"] = value
         save_config(CONFIG)
 
 if __name__ == "__main__":
-    StartUp = StartUp()
-    StartUp.enabled = True
-    print(StartUp.GetEnabled)
+    # StartUp = StartUpSys()
+    # StartUp.enabled = True
+    # print(StartUp.GetEnabled)
+    ...
