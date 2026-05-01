add8f1916fc0bfbede772af58a98f374f30961ae
ThainanViniciusKatchan <thaynanvinicyus1@gmail.com>
2026-05-01 14:43:53 -0400

    Lançamento da versão 1.1.2 com melhorias no código que ajudaram em futuras implementações no projeto, deixando o código mais legivel e menos bagunçado, os novos controladores do arquivo de configuraçõa estão funcionando ofícialmente com esse lançamento.

--- ui/Config_AutoDell.py
+++ ui/Config_AutoDell.py
@@ -2,7 +2,7 @@ import customtkinter
 import os
 import sys
 from ui.Centralizar_Janela import Centralizar_Janela
-from newUtils import delete, timeVerification
+from utils import delete, timeVerification
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.model import load_config, save_config
