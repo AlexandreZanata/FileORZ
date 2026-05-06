88bb87d8f9a0ba7b8215b3eff301ee47d88cc81e
Thainan <thaynanvinicyus1@gmail.com>
2026-05-06 10:01:57 -0400

    Estou adicionando a licença GPL ao projeto

--- ui/index.py
+++ ui/index.py
@@ -1,3 +1,22 @@
+"""
+    Copyright (C) 2026 Thainan Vinicius Katchan
+
+    This file is part of FileORZ.
+
+    FileORZ is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FileORZ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FileORZ.  If not, see <https://www.gnu.org/licenses/
+"""
+
 import customtkinter
 import os
 import sys
@@ -8,9 +27,6 @@ from ui.Select_Folder import folder_select
 from ui.Time_Select import time_select
 from ui.btn import config_btn, start_btn
 import ctypes
-
-# TODO:
-#   Finalizar a refatoração do código melhorando a escrita do front-end
 
 from utils import folder, timeVerification
 
