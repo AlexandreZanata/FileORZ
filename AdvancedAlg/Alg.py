559bdf8a4158b4f258111fdebbfa9870db8baa97
Thainan <thaynanvinicyus1@gmail.com>
2026-05-12 13:55:57 -0400

    Melhoria no código que realiza a leitura dos arquivos .pdf organizar, além que adicionar validação para saber se a organização avançada está ligada ou não, dessa forma o sistema ira organizar conforme as configurações, melhoria no código do AutoBuild.py, FileORZ.py, Alg.py, AdvancedConfig.py

--- AdvancedAlg/Alg.py
+++ AdvancedAlg/Alg.py
@@ -5,9 +5,9 @@ import os
 from utils.model import load_config
 from utils import folder
 
-CONFIG = load_config("AdvancedAlg", "Key_Words")
+CONFIG = load_config("dist", "Key_Words")
 
-diretorio_base: str = folder.Folder().Getfolder
+diretorio_base: str = folder.Folder().Getfolder + "\\"
 print(diretorio_base)
 
 def processar_texto():
@@ -17,11 +17,12 @@ def processar_texto():
         caminho_completo = os.path.join(diretorio_base, file)
         if not file.endswith(".pdf"):
             continue
-        with pypdf.PdfReader(diretorio_base+file) as pdf:
+        with pypdf.PdfReader(caminho_completo) as pdf:
             try:
-                for pagina in pdf:
+                for pagina in pdf.pages:
                     # Pega o texto e joga pra maiúsculo pra não ter erro de case
-                    texto_completo = pagina.get_text().upper()
+                    texto_completo = pagina.extract_text()
+                    texto_completo = texto_completo.upper()
             except Exception as e:
                 print(f"Não foi possivel ler o arquivo PDF {e}")
 
@@ -33,14 +34,13 @@ def processar_texto():
                 if palavra.upper() in texto_completo:
                     print(f"\nEncontrado: {palavra} ({tipo})")
                     pasta_destino = os.path.join(diretorio_base, tipo)
-                    Path(pasta_destino).mkdir(parents=True, exist_ok=True)
-                    destino_arquivo = os.path.join(pasta_destino, file)
                     Path(diretorio_base+tipo).mkdir(parents=True, exist_ok=True)
+                    destino_arquivo = os.path.join(pasta_destino, file)
                     if os.path.exists(destino_arquivo):
                         print(f"Arquivo {file} não movido pois já existe na pasta {tipo}")
                     else:
                         try:
-                            shutil.move(str(diretorio_base + file), str(diretorio_base+tipo))  # Move o arquivo para a pasta nova
+                            shutil.move(str(caminho_completo), str(destino_arquivo))  # Move o arquivo para a pasta nova
                             movido = True
                             print(f"Sucesso: Arquivo {file} movido para {pasta_destino}")
                         except Exception as e:
